0de2ef47e9268e596f908a98d56acfa613c58fa8
NahidHassan12 <76515481+NahidHassan12@users.noreply.github.com>
2021-09-13 10:32:57 +0600

    Update Button.dart

--- Day_6/Button.dart
+++ Day_6/Button.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+class MyRotWidget extends StatelessWidget {
+  const MyRotWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appbar: Appbar(
+        title: Text("ButtonApp"),
+        )
+        body: Center(
+          child: Center(
+            child: ElevatedButton)
+
+
+            ,)
+          ,)
+          )
+        ,)
+        )
+      )
+      )
+      
+    );
+  }
+}
+
+class Appbar {
+}
 
